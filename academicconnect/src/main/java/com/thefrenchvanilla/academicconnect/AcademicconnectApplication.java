1a62e21a9b8dd5566a54da40e3cf679da77e0ad2
steven.tsao <steven.tsao@teejlab.com>
2021-10-09 21:54:24 -0700

    Initialize project

---
+++ academicconnect/src/main/java/com/thefrenchvanilla/academicconnect/AcademicconnectApplication.java
@@ -0,0 +1,13 @@
+package com.thefrenchvanilla.academicconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicconnectApplication.class, args);
+	}
+
+}
---
+++ academicconnect/src/test/java/com/thefrenchvanilla/academicconnect/AcademicconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefrenchvanilla.academicconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
