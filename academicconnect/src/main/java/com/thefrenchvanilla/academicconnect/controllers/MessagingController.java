733af5e1f8fe10336c01bcfc928f5130beff77aa
steven.tsao <steven.tsao@teejlab.com>
2021-10-10 22:47:18 -0700

    Added pages for feed, messaging, and profile

---
+++ academicconnect/src/main/java/com/thefrenchvanilla/academicconnect/controllers/MessagingController.java
@@ -0,0 +1,15 @@
+package com.thefrenchvanilla.academicconnect.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MessagingController {
+
+	@RequestMapping({"/messaging", "/messaging/index", "/messaging/index.html", "/messaging.html"})
+    public String index(){
+
+        return "messaging/index";
+    }
+
+}
