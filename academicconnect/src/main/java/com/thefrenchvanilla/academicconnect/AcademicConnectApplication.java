99255b21a05e2e3551cb3c35a57158646700bfcd
steven.tsao <steven.tsao@teejlab.com>
2021-10-10 22:08:10 -0700

    Added welcome page

--- academicconnect/src/main/java/com/thefrenchvanilla/academicconnect/AcademicConnectApplication.java
+++ academicconnect/src/main/java/com/thefrenchvanilla/academicconnect/AcademicConnectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcademicconnectApplication {
+public class AcademicConnectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AcademicconnectApplication.class, args);
+		SpringApplication.run(AcademicConnectApplication.class, args);
 	}
 
 }
