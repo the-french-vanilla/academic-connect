a6545c2240ce0a038fe9601f6842053e87162d9f
steven.tsao <steven.tsao@teejlab.com>
2021-11-09 02:04:46 -0800

    Added comments about the APIs to implement

---
+++ academicconnect/src/main/java/com/thefrenchvanilla/academicconnect/controller/MessagingPageController.java
@@ -0,0 +1,26 @@
+package com.thefrenchvanilla.academicconnect.controller;
+
+import com.thefrenchvanilla.academicconnect.service.MapValidationErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/messaging_page")
+@CrossOrigin
+public class MessagingPageController {
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+
+    // get contacts
+    
+    // get messages
+    
+    // get contact details
+    
+    // send messages
+    
+    // display notifications
+    
+}
---
+++ academicconnect/src/main/java/com/thefrenchvanilla/academicconnect/controller/UserProfilePageController.java
@@ -0,0 +1,39 @@
+package com.thefrenchvanilla.academicconnect.controller;
+
+import com.thefrenchvanilla.academicconnect.service.MapValidationErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user_profile_page")
+@CrossOrigin
+public class UserProfilePageController {
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+
+    // get user profile
+    
+    // update user profile
+    
+    // get publications
+    
+    // get connections
+    
+    // get groups
+    
+    // get posts
+    
+    // create posts
+    
+    // update posts
+    
+    // comment posts
+    
+    // react to posts
+    
+    // delete posts
+    
+    // display notifications
+}
